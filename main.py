2a7c22749347550655358de4ca7ee7745f28c9ee
pinooxd <pinooxd@gmail.com>
2021-05-07 14:30:44 -0700

    first commit

--- main.py
+++ main.py
@@ -1,51 +1,39 @@
 import tkinter as tk
-import PyPDF2
 from PIL import Image, ImageTk
 from tkinter.filedialog import askopenfile
 
 root = tk.Tk()
-
-canvas = tk.Canvas(root, width=600, height=300)
+canvas = tk.Canvas(root, width=500, height=500)
 canvas.grid(columnspan=3, rowspan=3)
 
-# logo
-logo = Image.open('logo.png')
-logo = ImageTk.PhotoImage(logo)
-logo_label = tk.Label(image=logo)
-logo_label.image = logo
-logo_label.grid(column=1, row=0)
+# logo = Image.open('logo.png')
+# bg = ImageTk.PhotoImage(logo)
+# bg_label = tk.Label(image=bg)
+# bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-# instructions
-instructions = tk.Label(root, text="Select a PDF file on your computer to extract all its text", font="Raleway")
-instructions.grid(columnspan=3, column=0, row=1)
+#canvas.create_image(0, 0, image=bg, anchor="nw")
+canvas.configure(bg='#707698')
 
 
-def open_file():
-    browse_text.set("loading...")
-    file = askopenfile(parent=root, mode='rb', title="Choose a file", filetype=[("Pdf file", "*.pdf")])
-    if file:
-        read_pdf = PyPDF2.PdfFileReader(file)
-        page = read_pdf.getPage(0)
-        page_content = page.extractText()
-
-        # text box
-        text_box = tk.Text(root, height=10, width=50, padx=15, pady=15)
-        text_box.insert(1.0, page_content)
-        text_box.tag_configure("center", justify="center")
-        text_box.tag_add("center", 1.0, "end")
-        text_box.grid(column=1, row=3)
-
-        browse_text.set("Browse")
+def textin():
+    txt = text_box.get(1.0, "end-1c")
+    resp['text'] = txt
 
 
-# browse button
+# input text
+text_box = tk.Text(root, height=3, width=40, padx=15, pady=15)
+
+text_box.grid(column=0, row=2)
+
+# send button
 browse_text = tk.StringVar()
-browse_btn = tk.Button(root, textvariable=browse_text, command=lambda: open_file(), font="Raleway", bg="#20bebe",
-                       fg="white", height=2, width=15)
-browse_text.set("Browse")
-browse_btn.grid(column=1, row=2)
+browse_btn = tk.Button(root, textvariable=browse_text, command=textin, font="Raleway", bg="#171A2B",
+                       fg="white", height=2, width=9)
+browse_text.set("Send")
+browse_btn.grid(column=2, row=2)
 
-canvas = tk.Canvas(root, width=600, height=250)
-canvas.grid(columnspan=3)
+# response
+resp = tk.Label(root, text='Pregunta...?!', bg='#707698', font='Helvetica 14')
+resp.grid(column=0, row=0)
 
 root.mainloop()
